f509e45a85835d3c6d944eb69c89ad08e39cf800
Kevin Old <kevin@cypress.io>
2021-07-30 09:58:35 -0500

    wip - hard code specific content directories

--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -1,9 +1,13 @@
 import fs from 'fs'
 import path from 'path'
 
-// CONTENT_PATH is useful when you want to get the path to a specific file
 export const CONTENT_PATH = path.join(process.cwd(), 'content')
 export const TUTORIAL_PATH = path.join(process.cwd(), 'content/tutorial')
+
+export const TESTING_APP_PATH = path.join(process.cwd(), 'content/testing-your-first-app')
+export const TESTING_FOUNDATIONS_PATH = path.join(process.cwd(), 'content/testing-foundations')
+export const CYPRESS_FUNDAMENTALS_PATH = path.join(process.cwd(), 'content/cypress-fundamentals')
+export const REALWORLD_EXAMPLES_PATH = path.join(process.cwd(), 'content/real-world-examples')
 
 // contentFilePaths is the list of all mdx files inside the CONTENT_PATH directory
 export const contentFilePaths = fs
