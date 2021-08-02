54c521b48cdec82cdf5522c7a56fb276d58ee8ae
Kevin Old <kevin@cypress.io>
2021-08-02 14:56:39 -0500

    wip - render lessons data from learn.json in dynamic section page

--- pages/[section]/index.js
+++ pages/[section]/index.js
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import learnJson from "../../learn.json"
 
-export default function SectionPage({ section }) {
+export default function SectionPage({ section, title, lessons }) {
   const router = useRouter()
   //const { section } = router.query
 
@@ -9,20 +11,34 @@ export default function SectionPage({ section }) {
       <span>{router.query.section}</span>
       <br />
       <span>{section}</span>
+      <span>{title}</span>
+      <ul>
+          {lessons.map((lesson) => (
+            <li key={lesson.slug}>
+              <Link
+                as={`/${section}/${lesson.slug}`}
+                href={`/[section]/[slug]`}
+              >
+                <a>{lesson.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
     </>
   )
 }
 
 export async function getStaticProps({ params }) {
+  const { title, children } = learnJson[params.section]
   return {
-    props: { section: params.section },
+    props: { section: params.section, lessons: children, title },
   }
 }
 
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { section: 'testing-your-first-application' } },
+      { params: { section: 'testing-your-first-application', lessons: [], title: "Testing Your First App" } },
     ],
     fallback: true,
   }
