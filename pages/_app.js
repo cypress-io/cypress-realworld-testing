7bafb2dfe33ff8fac443fc336cd7146914d15d0f
Robert Guss <robertguss@pm.me>
2021-08-02 14:21:01 -0400

    installed Inter font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <link
+        href="https://rsms.me/inter/inter.css"
+        rel="stylesheet"
+      />
+    </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
