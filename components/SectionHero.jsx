9664ea48c45071125913f44d0f9c7233dbcee73c
Robert Guss <robertguss@pm.me>
2021-08-03 12:09:19 -0400

    updated buttons to be round on the section page

--- components/SectionHero.jsx
+++ components/SectionHero.jsx
@@ -11,8 +11,8 @@ import {
   SupportIcon,
   ViewGridIcon,
   XIcon,
-} from '@heroicons/react/outline'
-import Image from 'next/image'
+} from "@heroicons/react/outline"
+import Image from "next/image"
 
 export default function SectionHero(props) {
   return (
@@ -21,29 +21,25 @@ export default function SectionHero(props) {
         <div className="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
           <div className="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
-              <span className="block xl:inline">{props.title}</span>{' '}
+              <span className="block xl:inline">{props.title}</span>{" "}
               {/* <span className="block text-indigo-600 xl:inline">online business</span> */}
             </h1>
             <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
               {props.description}
             </p>
             <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
-              <div className="rounded-md shadow">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
-                >
-                  Start Lesson
-                </a>
-              </div>
-              <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
-                >
-                  Subscribe to Updates
-                </a>
-              </div>
+              <button
+                type="button"
+                className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Subscribe to Updates
+              </button>
+              <button
+                type="button"
+                className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-indigo-600 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-5"
+              >
+                Subscribe to Updates
+              </button>
             </div>
           </div>
         </div>
