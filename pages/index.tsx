5f3d94d998db661f065069facdd472d34e68cae3
Robert Guss <robertguss@pm.me>
2021-08-04 15:11:12 -0400

    moved all section components into components/Section/

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,18 +29,6 @@ export default function Home({ content }) {
             ))}
           </ul>
         </main>
-
-        <footer className="flex items-center justify-center w-full h-24 border-t">
-          <a
-            className="flex items-center justify-center"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-          </a>
-        </footer>
       </div>
     </Layout>
   )
