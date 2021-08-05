cfcfb294960d3a823b3032a00ac8744c6931f787
Robert Guss <robertguss@pm.me>
2021-08-05 14:13:28 -0400

    removed unused imports from home page hero

--- components/Home/HomeHero.tsx
+++ components/Home/HomeHero.tsx
@@ -1,8 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from "react"
-import { Popover, Transition } from "@headlessui/react"
-import { XIcon } from "@heroicons/react/outline"
-
 export default function HomeHero() {
   return (
     <div className="relative bg-gray-50 overflow-hidden">
