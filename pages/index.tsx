f873d68466936d9f250f1a3eb312b0b7bfdd673c
Robert Guss <robertguss@pm.me>
2021-08-05 14:18:20 -0400

    created HomeSteps component and fixed lesson count alignment with section titles on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,11 +53,13 @@ export default function Home({ content, sections }) {
 
           {sections.map((section) => (
             <div key={content[section].title} className="my-12">
-              <div className="section-title">
+              <div className="section-title flex items-center">
                 <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                   {content[section].title}
                 </h1>
-                <span>{content[section].children.length} Lessons</span>
+                <span className="ml-8">
+                  {content[section].children.length} Lessons
+                </span>
               </div>
 
               <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
