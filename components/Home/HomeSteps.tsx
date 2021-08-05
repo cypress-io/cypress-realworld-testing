95366fe16ec4ece4fe58224a364f98aa027e8614
Robert Guss <robertguss@pm.me>
2021-08-05 14:41:56 -0400

    cleaned up comments and unused imports from home page

--- components/Home/HomeSteps.tsx
+++ components/Home/HomeSteps.tsx
@@ -1,130 +1,143 @@
 /* This example requires Tailwind CSS v2.0+ */
+import React from "react"
+import Slider from "react-slick"
+import HomeCard from "./HomeCard"
 import { CheckIcon } from "@heroicons/react/solid"
-
-const steps = [
-  {
-    name: "Create account",
-    description: "Vitae sed mi luctus laoreet.",
-    href: "#",
-    status: "complete",
-  },
-  {
-    name: "Profile information",
-    description: "Cursus semper viverra facilisis et et some more.",
-    href: "#",
-    status: "current",
-  },
-  {
-    name: "Business information",
-    description: "Penatibus eu quis ante.",
-    href: "#",
-    status: "upcoming",
-  },
-]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
 }
 
-export default function SectionSteps(props) {
+export default function HomeSteps(props) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  }
+
   return (
     <nav aria-label="Progress">
       <ol className="overflow-hidden">
-        {props.lessons.map((lesson, index) => (
+        {props.sections.map((section, index) => (
           <li
-            key={lesson.title}
+            key={section}
             className={classNames(
-              index !== steps.length - 1 ? "pb-10" : "",
+              index !== props.sections.length - 1 ? "pb-10" : "",
               "relative"
             )}
           >
-            {lesson.status === "Completed" ? (
+            {props.content[section].status === "Completed" ? (
               <>
-                {index !== props.lessons.length - 1 ? (
+                {index !== props.sections.length - 1 ? (
                   <div
                     className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
                     aria-hidden="true"
                   />
                 ) : null}
-                <a
-                  href={lesson.href}
-                  className="relative flex items-start group"
-                >
-                  <span className="h-9 flex items-center">
-                    <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
-                      <CheckIcon
-                        className="w-5 h-5 text-white"
-                        aria-hidden="true"
-                      />
-                    </span>
+                <span className="h-9 flex items-center">
+                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
+                    <CheckIcon
+                      className="w-5 h-5 text-white"
+                      aria-hidden="true"
+                    />
                   </span>
-                  {/* <span className="ml-4 min-w-0 flex flex-col">
-                    <span className="text-xs font-semibold tracking-wide uppercase">
-                      {lesson.name}
+                </span>
+                <div key={props.content[section].title} className="my-12">
+                  <div className="section-title flex items-center">
+                    <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                      {props.content[section].title}
+                    </h1>
+                    <span className="ml-8">
+                      {props.content[section].children.length} Lessons
                     </span>
-                    <span className="text-sm text-gray-500">
-                      {lesson.description}
-                    </span>
-                  </span> */}
-                  <SectionCard lesson={lesson} />
-                </a>
+                  </div>
+
+                  <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                    {props.content[section].description}
+                  </p>
+
+                  <Slider {...settings}>
+                    {props.content[section].children.map((lesson) => (
+                      <div key={lesson.title}>
+                        <HomeCard lesson={lesson} />
+                      </div>
+                    ))}
+                  </Slider>
+                </div>
               </>
-            ) : lesson.status === "Current" ? (
+            ) : props.content[section].status === "Current" ? (
               <>
-                {index !== props.lessons.length - 1 ? (
+                {index !== props.sections.length - 1 ? (
                   <div
                     className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
                     aria-hidden="true"
                   />
                 ) : null}
-                <a
-                  href={lesson.href}
-                  className="relative flex items-start group"
-                  aria-current="step"
-                >
-                  <span className="h-9 flex items-center" aria-hidden="true">
-                    <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
-                      <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
-                    </span>
+                <span className="h-9 flex items-center" aria-hidden="true">
+                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
+                    <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
                   </span>
-                  {/* <span className="ml-4 min-w-0 flex flex-col">
-                    <span className="text-xs font-semibold tracking-wide uppercase text-indigo-600">
-                      {lesson.name}
+                </span>
+                <div key={props.content[section].title} className="my-12">
+                  <div className="section-title flex items-center">
+                    <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                      {props.content[section].title}
+                    </h1>
+                    <span className="ml-8">
+                      {props.content[section].children.length} Lessons
                     </span>
-                    <span className="text-sm text-gray-500">
-                      {lesson.description}
-                    </span>
-                  </span> */}
-                  <SectionCard lesson={lesson} />
-                </a>
+                  </div>
+
+                  <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                    {props.content[section].description}
+                  </p>
+
+                  <Slider {...settings}>
+                    {props.content[section].children.map((lesson) => (
+                      <div key={lesson.title}>
+                        <HomeCard lesson={lesson} />
+                      </div>
+                    ))}
+                  </Slider>
+                </div>
               </>
             ) : (
               <>
-                {index !== props.lessons.length - 1 ? (
+                {index !== props.sections.length - 1 ? (
                   <div
                     className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
                     aria-hidden="true"
                   />
                 ) : null}
-                <a
-                  href={lesson.href}
-                  className="relative flex items-start group"
-                >
-                  <span className="h-9 flex items-center" aria-hidden="true">
-                    <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
-                      <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300" />
-                    </span>
+                <span className="h-9 flex items-center" aria-hidden="true">
+                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
+                    <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300" />
                   </span>
-                  {/* <span className="ml-4 min-w-0 flex flex-col">
-                    <span className="text-xs font-semibold tracking-wide uppercase text-gray-500">
-                      {lesson.name}
+                </span>
+                <div key={props.content[section].title} className="my-12">
+                  <div className="section-title flex items-center">
+                    <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                      {props.content[section].title}
+                    </h1>
+                    <span className="ml-8">
+                      {props.content[section].children.length} Lessons
                     </span>
-                    <span className="text-sm text-gray-500">
-                      {lesson.description}
-                    </span>
-                  </span> */}
-                  <SectionCard lesson={lesson} />
-                </a>
+                  </div>
+
+                  <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                    {props.content[section].description}
+                  </p>
+
+                  <Slider {...settings}>
+                    {props.content[section].children.map((lesson) => (
+                      <div key={lesson.title}>
+                        <HomeCard lesson={lesson} />
+                      </div>
+                    ))}
+                  </Slider>
+                </div>
               </>
             )}
           </li>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,10 @@
-// import fs from "fs"
-// import matter from "gray-matter"
-import React from "react"
-import Slider from "react-slick"
 import Head from "next/head"
-// import Link from "next/link"
-// import path from "path"
 import Layout from "../components/Layout"
 import HomeHero from "../components/Home/HomeHero"
-import HomeCard from "../components/Home/HomeCard"
-// import { allContentFilePaths, CONTENT_PATH } from "../utils/mdxUtils"
+import HomeSteps from "../components/Home/HomeSteps"
 import learnJson from "../learn.json"
 
 export default function Home({ content, sections }) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  }
   return (
     <Layout>
       <HomeHero />
@@ -38,63 +24,12 @@ export default function Home({ content, sections }) {
         </Head>
 
         <main className="flex flex-col w-full flex-1 px-20">
-          {/* <ul>
-            {content.map((lesson) => (
-              <li key={lesson.filePath}>
-                <Link
-                  as={`/${lesson.filePath.replace(/\.mdx?$/, "")}`}
-                  href={`/[section]/[slug]`}
-                >
-                  <a>{lesson.data.title}</a>
-                </Link>
-              </li>
-            ))}
-          </ul> */}
-
-          {sections.map((section) => (
-            <div key={content[section].title} className="my-12">
-              <div className="section-title flex items-center">
-                <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                  {content[section].title}
-                </h1>
-                <span className="ml-8">
-                  {content[section].children.length} Lessons
-                </span>
-              </div>
-
-              <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
-                {content[section].description}
-              </p>
-
-              <Slider {...settings}>
-                {content[section].children.map((lesson) => (
-                  <div key={lesson.title}>
-                    <HomeCard lesson={lesson} />
-                  </div>
-                ))}
-              </Slider>
-            </div>
-          ))}
+          <HomeSteps sections={sections} content={content} />
         </main>
       </div>
     </Layout>
   )
 }
-
-// export function getStaticProps() {
-//   const content = allContentFilePaths.map((filePath) => {
-//     const source = fs.readFileSync(path.join(CONTENT_PATH, filePath))
-//     const { content, data } = matter(source)
-
-//     return {
-//       content,
-//       data,
-//       filePath,
-//     }
-//   })
-
-//   return { props: { content } }
-// }
 
 export async function getStaticProps() {
   const sections = Object.keys(learnJson)
