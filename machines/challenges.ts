ba9ca07c10534bf443bdd216b465280921cccd41
Kevin Old <kevin@cypress.io>
2021-08-06 11:05:28 -0500

    update on to be specific to SUBMIT_ANSWER

--- machines/challenges.ts
+++ machines/challenges.ts
@@ -46,7 +46,7 @@ export const challengeMachine = createMachine(
       },
       validating: {
         on: {
-          "": [
+          SUBMIT_ANSWER: [
             {
               target: "answeredCorrectly",
               cond: validateAnswer,
