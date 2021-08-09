f5e52fd4f4e6e2fd741930287136d5648f946692
Robert Guss <robertguss@pm.me>
2021-08-09 10:09:52 -0400

    added tailwind forms package

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [require("@tailwindcss/aspect-ratio"), require('@tailwindcss/forms')],
 }
