385f4448f4777806e5b8af2ac4ea343b95ae296d
Robert Guss <robertguss@pm.me>
2021-08-09 11:20:22 -0400

    cleaned up lesson hero. removed svg dots and email signup form

--- components/Lesson/LessonHero.tsx
+++ components/Lesson/LessonHero.tsx
@@ -28,52 +28,6 @@ const navigation = [
 export default function LessonHero(props) {
   return (
     <div className="relative bg-white overflow-hidden">
-      <div
-        className="hidden lg:block lg:absolute lg:inset-0"
-        aria-hidden="true"
-      >
-        <svg
-          className="absolute top-0 left-1/2 transform translate-x-64 -translate-y-8"
-          width={640}
-          height={784}
-          fill="none"
-          viewBox="0 0 640 784"
-        >
-          <defs>
-            <pattern
-              id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
-              x={118}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x={0}
-                y={0}
-                width={4}
-                height={4}
-                className="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            y={72}
-            width={640}
-            height={640}
-            className="text-gray-50"
-            fill="currentColor"
-          />
-          <rect
-            x={118}
-            width={404}
-            height={784}
-            fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)"
-          />
-        </svg>
-      </div>
-
       <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
         <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
@@ -88,36 +42,6 @@ export default function LessonHero(props) {
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                 {props.lessonData.description}
               </p>
-              <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-                <p className="text-base font-medium text-gray-900">
-                  Sign up to get notified when it’s ready.
-                </p>
-                <form action="#" method="POST" className="mt-3 sm:flex">
-                  <label htmlFor="email" className="sr-only">
-                    Email
-                  </label>
-                  <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    className="block w-full py-3 text-base rounded-md placeholder-gray-500 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:flex-1 border-gray-300"
-                    placeholder="Enter your email"
-                  />
-                  <button
-                    type="submit"
-                    className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
-                  >
-                    Notify me
-                  </button>
-                </form>
-                <p className="mt-3 text-sm text-gray-500">
-                  We care about the protection of your data. Read our
-                  <a href="#" className="font-medium text-gray-900 underline">
-                    Privacy Policy
-                  </a>
-                  .
-                </p>
-              </div>
             </div>
             <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
               <svg
