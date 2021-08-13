c59036d9bd3e5212c982ea9b403b641a6ce018c3
Robert Guss <robertguss@pm.me>
2021-08-13 11:36:39 -0400

    styled multiple choice challenge comp

--- components/Lesson/MultipleChoiceChallenge.tsx
+++ components/Lesson/MultipleChoiceChallenge.tsx
@@ -1,6 +1,40 @@
 import { useRouter } from "next/router"
 import { useActor } from "@xstate/react"
 
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+  ScaleIcon,
+} from "@heroicons/react/outline"
+
+const features = [
+  {
+    name: "Competitive exchange rates",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: GlobeAltIcon,
+  },
+  {
+    name: "No hidden fees",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: ScaleIcon,
+  },
+  {
+    name: "Transfers are instant",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: LightningBoltIcon,
+  },
+  {
+    name: "Mobile notifications",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+    icon: AnnotationIcon,
+  },
+]
+
 export default function LessonChallenge(props) {
   const router = useRouter()
   const { section, slug } = router.query
@@ -11,73 +45,55 @@ export default function LessonChallenge(props) {
   )
 
   return (
-    <form className="space-y-8 divide-y divide-gray-200 mb-52">
-      <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-        <div className="divide-y divide-gray-200 pt-8 space-y-6 sm:pt-10 sm:space-y-5">
-          <div className="space-y-6 sm:space-y-5 divide-y divide-gray-200">
-            <div className="pt-6 sm:pt-5">
-              <div role="group" aria-labelledby="label-email">
-                <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
-                  <div>
-                    <div
-                      className="text-base font-medium text-gray-900 sm:text-sm sm:text-gray-700"
-                      id="label-email"
-                    >
-                      {props.lessonData.challenges[0].question}:
-                      <br />
-                      <br />
-                      {isLessonComplete ? "CORRECT" : "INCORRECT"}
-                    </div>
-                  </div>
-                  <div className="mt-4 sm:mt-0 sm:col-span-2">
-                    <div className="max-w-lg space-y-4">
-                      {/* Answer */}
+    <div className="py-12 bg-white">
+      <hr className="mb-6" />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+            Unlock the Next Lesson
+          </h2>
+          <p className="mt-6 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            {props.lessonData.challenges[0].question}
+          </p>
+          {isLessonComplete ? "CORRECT" : "INCORRECT"}
+        </div>
 
-                      {props.lessonData.challenges[0].answers.map(
-                        (answer, index) => (
-                          <div
-                            className="relative flex items-start"
-                            key={index}
-                          >
-                            <div className="flex items-center h-5">
-                              <input
-                                id="comments"
-                                name="comments"
-                                type="checkbox"
-                                className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                                onClick={() => {
-                                  progressSend({
-                                    type: "SUBMIT_ANSWER",
-                                    id: `${section}/${slug}`,
-                                    challengeIndex: 0,
-                                    userAnswerIndex: index,
-                                  })
-                                }}
-                              />
-                            </div>
-                            <div className="ml-3 text-sm">
-                              <label
-                                htmlFor="comments"
-                                className="font-medium text-gray-700"
-                              >
-                                {answer}
-                              </label>
-                              {/* <p className="text-gray-500">
-                            Get notified when someones posts a comment on a
-                            posting.
-                          </p> */}
-                            </div>
-                          </div>
-                        )
-                      )}
-                    </div>
+        <div className="mt-10">
+          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            {props.lessonData.challenges[0].answers.map((answer, index) => (
+              <div key={index} className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-8 w-8">
+                    <input
+                      id={`${answer}-${index}`}
+                      name="answer"
+                      type="checkbox"
+                      className="focus:ring-indigo-500 text-indigo-600 border-gray-300 rounded h-8 w-8"
+                      onClick={() => {
+                        progressSend({
+                          type: "SUBMIT_ANSWER",
+                          id: `${section}/${slug}`,
+                          challengeIndex: 0,
+                          userAnswerIndex: index,
+                        })
+                      }}
+                    />
                   </div>
-                </div>
+                  <label
+                    htmlFor={`${answer}-${index}`}
+                    className="ml-16 text-lg leading-6 font-medium text-gray-900"
+                  >
+                    {answer}
+                  </label>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  {/* {feature.description} */}
+                </dd>
               </div>
-            </div>
-          </div>
+            ))}
+          </dl>
         </div>
       </div>
-    </form>
+    </div>
   )
 }
