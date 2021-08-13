a6a32bc5f024d202a51da1696bfe968ec5a96d5b
Robert Guss <robertguss@pm.me>
2021-08-13 12:13:15 -0400

    centered ff challenge

--- components/Lesson/FreeFormChallenge.tsx
+++ components/Lesson/FreeFormChallenge.tsx
@@ -21,44 +21,46 @@ export default function LessonChallenge(props) {
   }
 
   return (
-    <div className="py-12 bg-white">
+    <>
       <hr className="mb-6" />
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Unlock the Next Lesson
-          </h2>
-          <p className="mt-6 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            {props.lessonData.challenges[0].question}
-          </p>
-          {isLessonComplete ? "CORRECT" : "INCORRECT"}
-        </div>
+      <div className="py-12 bg-white lg:grid lg:grid-cols-12 lg:gap-8">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 lg:col-span-9 xl:col-span-7 xl:col-start-3">
+          <div className="lg:text-center">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+              Unlock the Next Lesson
+            </h2>
+            <p className="mt-6 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              {props.lessonData.challenges[0].question}
+            </p>
+            {isLessonComplete ? "CORRECT" : "INCORRECT"}
+          </div>
 
-        <div className="mt-10 max-w-sm mx-auto">
-          <form
-            onSubmit={handleSubmit}
-            className="mt-5 sm:flex sm:items-center"
-          >
-            <div className="w-full sm:max-w-xs">
-              <label htmlFor="answer" className="sr-only">
-                Answer
-              </label>
-              <input
-                type="text"
-                name="answer"
-                id="answer"
-                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-              />
-            </div>
-            <button
-              type="submit"
-              className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          <div className="mt-10 max-w-sm mx-auto">
+            <form
+              onSubmit={handleSubmit}
+              className="mt-5 sm:flex sm:items-center"
             >
-              Submit
-            </button>
-          </form>
+              <div className="w-full sm:max-w-xs">
+                <label htmlFor="answer" className="sr-only">
+                  Answer
+                </label>
+                <input
+                  type="text"
+                  name="answer"
+                  id="answer"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <button
+                type="submit"
+                className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
