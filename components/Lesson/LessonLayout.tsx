0acf24d334b97824559f049e9ed5795f91f41d92
Robert Guss <robertguss@pm.me>
2021-08-13 10:55:23 -0400

    added tailwind ui content styles to lessson content

--- components/Lesson/LessonLayout.tsx
+++ components/Lesson/LessonLayout.tsx
@@ -1,6 +1,8 @@
 import LessonToc from "../../components/Lesson/LessonToc"
 import LessonSteps from "../../components/Lesson/LessonSteps"
+import Content from "../../components/Lesson/Content"
 import { MDXRemote } from "next-mdx-remote"
+import styles from "../../styles/lesson-content.module.css"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
@@ -10,7 +12,8 @@ export default function LessonLayout(props) {
   return (
     <div className="min-h-screen">
       <div className="py-6">
-        <div className="max-w-3xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
+        <div className="max-w-3xl mx-auto sm:px-6 lg:max-w-full lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
+          {/* Table of Content */}
           <div className="hidden lg:block lg:col-span-3 xl:col-span-2">
             <nav
               aria-label="Sidebar"
@@ -19,9 +22,15 @@ export default function LessonLayout(props) {
               <LessonToc navigation={props.toc} />
             </nav>
           </div>
-          <main className="lesson-content lg:col-span-9 xl:col-span-6">
-            <div className="prose lg:prose-xl">
-              <MDXRemote {...props.source} components={props.components} />
+
+          {/* Content */}
+          <main className="lesson-content lg:col-span-9 xl:col-span-7">
+            <div className="relative pb-16 bg-white overflow-hidden">
+              <div className="relative px-4 sm:px-6 lg:px-8">
+                <div className="prose prose-indigo prose-lg text-gray-500 mx-auto">
+                  <MDXRemote {...props.source} components={props.components} />
+                </div>
+              </div>
             </div>
 
             {/* Next Lesson Button */}
@@ -36,7 +45,9 @@ export default function LessonLayout(props) {
               </div>
             )}
           </main>
-          <aside className="hidden xl:block xl:col-span-4">
+
+          {/* Progress Steps */}
+          <aside className="hidden xl:block xl:col-span-3">
             <div className="sticky top-6 space-y-4">
               <LessonSteps sectionLessons={props.sectionLessons} />
             </div>
