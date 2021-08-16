45ca75de497785c857186eb9e146d8b9b5199479
Robert Guss <robertguss@pm.me>
2021-08-16 14:00:16 -0400

    finished homepage responsive styles for mobile

--- components/Home/HomeSteps.tsx
+++ components/Home/HomeSteps.tsx
@@ -53,9 +53,10 @@ export default function HomeSteps(props) {
               "relative"
             )}
           >
-            {/* TODO: Refactor and try to consense/simplify */}
+            {/* TODO: Refactor and try to condense/simplify */}
             {props.content[section].status === "Completed" ? (
               <>
+                {/* Solid Line that connects the checkmarks */}
                 {index !== props.sections.length - 1 ? (
                   <div
                     className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
@@ -106,36 +107,43 @@ export default function HomeSteps(props) {
               </>
             ) : props.content[section].status === "Current" ? (
               <>
+                {/* Solid Line that connects the checkmarks */}
                 {index !== props.sections.length - 1 ? (
                   <div
                     className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
                     aria-hidden="true"
                   />
                 ) : null}
+
+                {/* Check Icon */}
                 <span className="h-9 flex items-center" aria-hidden="true">
                   <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
                     <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
                   </span>
                 </span>
+
                 <div
                   key={props.content[section].title}
-                  className="my-12 lg:mx-24"
+                  className="lg:my-12 mx-12 lg:mx-24"
                 >
-                  <div className="section-title flex items-center">
+                  {/* Section Title */}
+                  <div className="section-title ml-8 flex flex-col lg:items-center">
                     <a href={section}>
                       <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                         {props.content[section].title}
                       </h1>
                     </a>
-                    <span className="ml-8">
+                    <span className="mt-4 lg:ml-8">
                       {props.content[section].lessons.length} Lessons
                     </span>
                   </div>
 
-                  <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                  {/* Section Description */}
+                  <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
                     {props.content[section].description}
                   </p>
 
+                  {/* Slides */}
                   <Slider {...settings}>
                     {props.content[section].lessons.map((lesson) => (
                       <div key={lesson.title}>
@@ -147,36 +155,43 @@ export default function HomeSteps(props) {
               </>
             ) : (
               <>
+                {/* Solid Line that connects the checkmarks */}
                 {index !== props.sections.length - 1 ? (
                   <div
                     className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
                     aria-hidden="true"
                   />
                 ) : null}
+                {/* Check Icon */}
+
                 <span className="h-9 flex items-center" aria-hidden="true">
                   <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
                     <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300" />
                   </span>
                 </span>
+
                 <div
                   key={props.content[section].title}
-                  className="my-12 lg:mx-24"
+                  className="lg:my-12 mx-12 lg:mx-24"
                 >
-                  <div className="section-title flex items-center">
+                  {/* Section Title */}
+                  <div className="section-title ml-8 flex flex-col lg:items-center">
                     <a href={section}>
                       <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                         {props.content[section].title}
                       </h1>
                     </a>
-                    <span className="ml-8">
+                    <span className="mt-4 lg:ml-8">
                       {props.content[section].lessons.length} Lessons
                     </span>
                   </div>
 
-                  <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                  {/* Section Description */}
+                  <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
                     {props.content[section].description}
                   </p>
 
+                  {/* Slides */}
                   <Slider {...settings}>
                     {props.content[section].lessons.map((lesson) => (
                       <div key={lesson.title}>
