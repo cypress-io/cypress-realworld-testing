2513426f2830cd0cb867844b8dcf7b415ae981da
Robert Guss <robertguss@pm.me>
2021-08-16 14:25:57 -0400

    refactored home steps component

--- components/Home/HomeSteps.tsx
+++ components/Home/HomeSteps.tsx
@@ -53,155 +53,78 @@ export default function HomeSteps(props) {
               "relative"
             )}
           >
-            {/* TODO: Refactor and try to condense/simplify */}
-            {props.content[section].status === "Completed" ? (
-              <>
-                {/* Solid Line that connects the checkmarks */}
-                {index !== props.sections.length - 1 ? (
-                  <div
-                    className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-indigo-600"
+            {/* Solid Line that connects the checkmarks */}
+            {index !== props.sections.length - 1 ? (
+              <div
+                className={`-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full ${
+                  props.content[section].status === "Completed"
+                    ? "bg-indigo-600"
+                    : "bg-gray-300"
+                }`}
+                aria-hidden="true"
+              />
+            ) : null}
+
+            {/* "Completed" Check Icon */}
+            {props.content[section].status === "Completed" && (
+              <span className="h-9 flex items-center">
+                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
+                  <CheckIcon
+                    className="w-5 h-5 text-white"
                     aria-hidden="true"
                   />
-                ) : null}
-
-                {/* Check Icon */}
-                <span className="h-9 flex items-center">
-                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
-                    <CheckIcon
-                      className="w-5 h-5 text-white"
-                      aria-hidden="true"
-                    />
-                  </span>
                 </span>
-
-                <div
-                  key={props.content[section].title}
-                  className="lg:my-12 mx-12 lg:mx-24"
-                >
-                  {/* Section Title */}
-                  <div className="section-title ml-8 flex flex-col lg:items-center">
-                    <a href={section}>
-                      <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                        {props.content[section].title}
-                      </h1>
-                    </a>
-                    <span className="mt-4 lg:ml-8">
-                      {props.content[section].lessons.length} Lessons
-                    </span>
-                  </div>
-
-                  {/* Section Description */}
-                  <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
-                    {props.content[section].description}
-                  </p>
-
-                  {/* Slides */}
-                  <Slider {...settings}>
-                    {props.content[section].lessons.map((lesson) => (
-                      <div key={lesson.title}>
-                        <HomeCard lesson={lesson} section={section} />
-                      </div>
-                    ))}
-                  </Slider>
-                </div>
-              </>
-            ) : props.content[section].status === "Current" ? (
-              <>
-                {/* Solid Line that connects the checkmarks */}
-                {index !== props.sections.length - 1 ? (
-                  <div
-                    className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
-                    aria-hidden="true"
-                  />
-                ) : null}
-
-                {/* Check Icon */}
-                <span className="h-9 flex items-center" aria-hidden="true">
-                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
-                    <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
-                  </span>
-                </span>
-
-                <div
-                  key={props.content[section].title}
-                  className="lg:my-12 mx-12 lg:mx-24"
-                >
-                  {/* Section Title */}
-                  <div className="section-title ml-8 flex flex-col lg:items-center">
-                    <a href={section}>
-                      <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                        {props.content[section].title}
-                      </h1>
-                    </a>
-                    <span className="mt-4 lg:ml-8">
-                      {props.content[section].lessons.length} Lessons
-                    </span>
-                  </div>
-
-                  {/* Section Description */}
-                  <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
-                    {props.content[section].description}
-                  </p>
-
-                  {/* Slides */}
-                  <Slider {...settings}>
-                    {props.content[section].lessons.map((lesson) => (
-                      <div key={lesson.title}>
-                        <HomeCard lesson={lesson} section={section} />
-                      </div>
-                    ))}
-                  </Slider>
-                </div>
-              </>
-            ) : (
-              <>
-                {/* Solid Line that connects the checkmarks */}
-                {index !== props.sections.length - 1 ? (
-                  <div
-                    className="-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full bg-gray-300"
-                    aria-hidden="true"
-                  />
-                ) : null}
-                {/* Check Icon */}
-
-                <span className="h-9 flex items-center" aria-hidden="true">
-                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
-                    <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300" />
-                  </span>
-                </span>
-
-                <div
-                  key={props.content[section].title}
-                  className="lg:my-12 mx-12 lg:mx-24"
-                >
-                  {/* Section Title */}
-                  <div className="section-title ml-8 flex flex-col lg:items-center">
-                    <a href={section}>
-                      <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                        {props.content[section].title}
-                      </h1>
-                    </a>
-                    <span className="mt-4 lg:ml-8">
-                      {props.content[section].lessons.length} Lessons
-                    </span>
-                  </div>
-
-                  {/* Section Description */}
-                  <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
-                    {props.content[section].description}
-                  </p>
-
-                  {/* Slides */}
-                  <Slider {...settings}>
-                    {props.content[section].lessons.map((lesson) => (
-                      <div key={lesson.title}>
-                        <HomeCard lesson={lesson} section={section} />
-                      </div>
-                    ))}
-                  </Slider>
-                </div>
-              </>
+              </span>
             )}
+
+            {/* "Current" Check Icon */}
+            {props.content[section].status === "Current" && (
+              <span className="h-9 flex items-center" aria-hidden="true">
+                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
+                  <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
+                </span>
+              </span>
+            )}
+
+            {/* "Upcoming" Check Icon */}
+            {props.content[section].status === "Upcoming" && (
+              <span className="h-9 flex items-center" aria-hidden="true">
+                <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
+                  <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300" />
+                </span>
+              </span>
+            )}
+
+            <div
+              key={props.content[section].title}
+              className="lg:my-12 mx-12 lg:mx-24"
+            >
+              {/* Section Title */}
+              <div className="section-title ml-8 flex flex-col lg:items-center">
+                <a href={section}>
+                  <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+                    {props.content[section].title}
+                  </h1>
+                </a>
+                <span className="mt-4 lg:ml-8">
+                  {props.content[section].lessons.length} Lessons
+                </span>
+              </div>
+
+              {/* Section Description */}
+              <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                {props.content[section].description}
+              </p>
+
+              {/* Slides */}
+              <Slider {...settings}>
+                {props.content[section].lessons.map((lesson) => (
+                  <div key={lesson.title}>
+                    <HomeCard lesson={lesson} section={section} />
+                  </div>
+                ))}
+              </Slider>
+            </div>
           </li>
         ))}
       </ol>
