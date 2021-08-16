270a94d75e47241a427f7c0fd0100a5849b435ff
Robert Guss <robertguss@pm.me>
2021-08-16 10:13:21 -0400

    made homepage completed cards and slides responsivve

--- components/Home/HomeSteps.tsx
+++ components/Home/HomeSteps.tsx
@@ -53,6 +53,7 @@ export default function HomeSteps(props) {
               "relative"
             )}
           >
+            {/* TODO: Refactor and try to consense/simplify */}
             {props.content[section].status === "Completed" ? (
               <>
                 {index !== props.sections.length - 1 ? (
@@ -61,6 +62,8 @@ export default function HomeSteps(props) {
                     aria-hidden="true"
                   />
                 ) : null}
+
+                {/* Check Icon */}
                 <span className="h-9 flex items-center">
                   <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
                     <CheckIcon
@@ -69,25 +72,29 @@ export default function HomeSteps(props) {
                     />
                   </span>
                 </span>
+
                 <div
                   key={props.content[section].title}
-                  className="my-12 lg:mx-24"
+                  className="lg:my-12 mx-12 lg:mx-24"
                 >
-                  <div className="section-title flex items-center">
+                  {/* Section Title */}
+                  <div className="section-title ml-8 flex flex-col lg:items-center">
                     <a href={section}>
                       <h1 className="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                         {props.content[section].title}
                       </h1>
                     </a>
-                    <span className="ml-8">
+                    <span className="mt-4 lg:ml-8">
                       {props.content[section].lessons.length} Lessons
                     </span>
                   </div>
 
-                  <p className="mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
+                  {/* Section Description */}
+                  <p className="mt-3 ml-8 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-12">
                     {props.content[section].description}
                   </p>
 
+                  {/* Slides */}
                   <Slider {...settings}>
                     {props.content[section].lessons.map((lesson) => (
                       <div key={lesson.title}>
