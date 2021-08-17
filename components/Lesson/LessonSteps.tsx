ca28bfb35561bd8d6b944f830b435666bd610e24
Robert Guss <robertguss@pm.me>
2021-08-17 16:10:51 -0400

    refactored lessons steps comp

--- components/Lesson/LessonSteps.tsx
+++ components/Lesson/LessonSteps.tsx
@@ -21,7 +21,7 @@ export default function LessonSteps({ sectionLessons, progressState }) {
             {index !== sectionLessons.length - 1 ? (
               <div
                 className={`-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full ${
-                  lesson.status === "Completed"
+                  lesson.status === "completed"
                     ? "bg-indigo-600"
                     : "bg-gray-300"
                 }`}
@@ -30,8 +30,8 @@ export default function LessonSteps({ sectionLessons, progressState }) {
             ) : null}
 
             <a href={lesson.href} className="relative flex items-start group">
-              {/* "Completed" */}
-              {lesson.status === "Completed" && (
+              {/* "completed" */}
+              {lesson.status === "completed" && (
                 <span className="h-9 flex items-center">
                   <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
                     <CheckIcon
@@ -42,17 +42,8 @@ export default function LessonSteps({ sectionLessons, progressState }) {
                 </span>
               )}
 
-              {/* "Current" */}
-              {lesson.status === "Current" && (
-                <span className="h-9 flex items-center" aria-hidden="true">
-                  <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-indigo-600 rounded-full">
-                    <span className="h-2.5 w-2.5 bg-indigo-600 rounded-full" />
-                  </span>
-                </span>
-              )}
-
               {/* "Upcoming" */}
-              {lesson.status === "Upcoming" && (
+              {lesson.status === "upcoming" && (
                 <span className="h-9 flex items-center" aria-hidden="true">
                   <span className="relative z-10 w-8 h-8 flex items-center justify-center bg-white border-2 border-gray-300 rounded-full group-hover:border-gray-400">
                     <span className="h-2.5 w-2.5 bg-transparent rounded-full group-hover:bg-gray-300" />
