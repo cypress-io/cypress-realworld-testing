329945a9b643c61bc21e8d88dbbea77b61a6cc11
Robert Guss <robertguss@pm.me>
2021-08-17 14:51:23 -0400

    updated isLessonCompleted util function

--- utils/machineUtils.ts
+++ utils/machineUtils.ts
@@ -1,5 +1,11 @@
-import {includes} from "lodash/fp"
+import { findIndex } from "lodash/fp"
 
 export const isLessonCompleted = (progressState, lessonPath) => {
-  return includes(lessonPath, progressState.context.lessonsCompleted)
+  const lessonFound = findIndex({id: lessonPath, status: "completed"}, progressState.context.lessons)
+  
+  if (lessonFound !== -1) {
+    return true
+  } else {
+    return false
+  }
 }
