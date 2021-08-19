fe038b288d538606ce3cb4e39fb862271fdfbc7d
Robert Guss <robertguss@pm.me>
2021-08-19 13:35:05 -0400

    organized machine utils be alphabetical order

--- utils/machineUtils.ts
+++ utils/machineUtils.ts
@@ -1,12 +1,21 @@
 import { find, findIndex, get, gte } from "lodash/fp"
 import { Lesson } from "../types/common"
 
-export const isLessonCompleted = (progressState, lessonPath) => {
-  const [, lessonSlug] = lessonPath.split("/")
-  const lessons = getAllLessons(progressState, lessonPath)
+export const getAllLessons = (progressState, lessonPath) => {
+  const [sectionSlug] = lessonPath.split("/")
+  const section = getSection(progressState.context.learnData, sectionSlug)
 
-  return gte(findIndex({ slug: lessonSlug, status: "completed" }, lessons), 0)
+  return get("lessons", section)
 }
+
+export const findLesson = (lessons: object[], lessonSlug: string) =>
+  find({ slug: lessonSlug }, lessons)
+
+export const getChallenge = (lesson: Lesson, challengeIndex: string) =>
+  lesson.challenges[challengeIndex]
+
+export const getLessonIndex = (lessons: object[], lessonSlug: string) =>
+  findIndex({ slug: lessonSlug }, lessons)
 
 export const getSection = (learnData: object[], sectionSlug: string) =>
   find({ slug: sectionSlug }, learnData)
@@ -14,18 +23,9 @@ export const getSection = (learnData: object[], sectionSlug: string) =>
 export const getSectionIndex = (learnData: object[], sectionSlug: string) =>
   findIndex({ slug: sectionSlug }, learnData)
 
-export const findLesson = (lessons: object[], lessonSlug: string) =>
-  find({ slug: lessonSlug }, lessons)
+export const isLessonCompleted = (progressState, lessonPath) => {
+  const [, lessonSlug] = lessonPath.split("/")
+  const lessons = getAllLessons(progressState, lessonPath)
 
-export const getLessonIndex = (lessons: object[], lessonSlug: string) =>
-  findIndex({ slug: lessonSlug }, lessons)
-
-export const getChallenge = (lesson: Lesson, challengeIndex: string) =>
-  lesson.challenges[challengeIndex]
-
-export const getAllLessons = (progressState, lessonPath) => {
-  const [sectionSlug] = lessonPath.split("/")
-  const section = getSection(progressState.context.learnData, sectionSlug)
-
-  return get("lessons", section)
+  return gte(findIndex({ slug: lessonSlug, status: "completed" }, lessons), 0)
 }
