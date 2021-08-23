87b66fc0e2cc7683d373f93a24a0241bf2f21787
Robert Guss <robertguss@pm.me>
2021-08-23 14:22:38 -0400

    added test case for incorrect free form answer

--- cypress/integration/freeFormChallenge.spec.ts
+++ cypress/integration/freeFormChallenge.spec.ts
@@ -13,6 +13,16 @@ describe("Free Form Challenge", () => {
     cy.getBySel("next-lesson-button").should("be.visible")
     cy.getBySel("lesson-complete-1").should("have.class", "bg-indigo-600")
   })
+
+  it.only("it does not display the next lesson button when an answer is incorrect and does updates the progress sidebar", () => {
+    cy.get("#answer").type("cy.get('.new-todo')")
+    cy.get("button[type='submit']").click()
+    cy.getBySel("next-lesson-button").should("be.not.visible")
+    cy.getBySel("lesson-upcoming-1").should(
+      "have.class",
+      "bg-white border-2 border-gray-300"
+    )
+  })
 })
 
 export {}
