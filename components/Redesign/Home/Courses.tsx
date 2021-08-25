459c44a39c7aefaa6d45973d96aeeb22cbde021f
Robert Guss <robertguss@pm.me>
2021-08-25 14:04:16 -0400

    removed icons from courses section

--- components/Redesign/Home/Courses.tsx
+++ components/Redesign/Home/Courses.tsx
@@ -13,14 +13,6 @@ export default function Courses() {
           <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
               <div>
-                <div>
-                  <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                    <InboxIcon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </div>
                 <div className="mt-6">
                   <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
                     Testing Your First Application
@@ -61,14 +53,6 @@ export default function Courses() {
           <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
               <div>
-                <div>
-                  <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                    <SparklesIcon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </div>
                 <div className="mt-6">
                   <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
                     Testing Foundations
@@ -111,14 +95,6 @@ export default function Courses() {
           <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
               <div>
-                <div>
-                  <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                    <InboxIcon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </div>
                 <div className="mt-6">
                   <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
                     Cypress Fundamentals
@@ -159,14 +135,6 @@ export default function Courses() {
           <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
             <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
               <div>
-                <div>
-                  <span className="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                    <SparklesIcon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </div>
                 <div className="mt-6">
                   <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
                     Advanced Cypress Concepts
--- components/Redesign/Home/Footer.tsx
+++ components/Redesign/Home/Footer.tsx
@@ -63,18 +63,6 @@ export default function Footer() {
             </div>
           ))}
         </nav>
-        {/* <div className="mt-8 flex justify-center space-x-6">
-          {navigation.social.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-gray-400 hover:text-gray-500"
-            >
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
-          ))}
-        </div> */}
         <p className="mt-8 text-center text-base text-gray-400">
           &copy; 2021 Cypress. All rights reserved.
         </p>
