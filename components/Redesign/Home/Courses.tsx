08125a37f6940193431ce44c5150bcd3e9ad95dc
Robert Guss <robertguss@pm.me>
2021-08-26 13:29:50 -0400

    updated get started buttons in courses to navigate to the section page

--- components/Redesign/Home/Courses.tsx
+++ components/Redesign/Home/Courses.tsx
@@ -28,7 +28,7 @@ export default function Courses({ sections, content, progressService }) {
                       />
                       <div className="mt-6">
                         <a
-                          href="#"
+                          href={content[section].slug}
                           className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                         >
                           Get started
@@ -67,7 +67,7 @@ export default function Courses({ sections, content, progressService }) {
                       />
                       <div className="mt-6">
                         <a
-                          href="#"
+                          href={content[section].slug}
                           className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                         >
                           Get started
