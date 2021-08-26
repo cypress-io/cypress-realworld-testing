cbfb430ac29dfe2dc04791966f7848e0e78cf8f8
Robert Guss <robertguss@pm.me>
2021-08-26 14:09:32 -0400

    added search to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,6 +19,7 @@ import {
   UserGroupIcon,
   ViewGridIcon,
   XIcon,
+  SearchIcon,
 } from "@heroicons/react/outline"
 import { ChevronDownIcon } from "@heroicons/react/solid"
 
@@ -346,18 +347,28 @@ export default function Header() {
               </Popover>
             </Popover.Group>
             <div className="flex items-center md:ml-12">
-              <a
-                href="#"
-                className="text-base font-medium text-gray-500 hover:text-gray-900"
-              >
-                Sign in
-              </a>
-              <a
-                href="#"
-                className="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-              >
-                Sign up
-              </a>
+              {/* Search */}
+              <div className="md:flex items-center justify-end w-full max-w-sm">
+                <div className="flex-1 min-w-0">
+                  <div className="w-full">
+                    <label htmlFor="desktop-search" className="sr-only">
+                      Search
+                    </label>
+                    <div className="relative focus-within:text-gray-600">
+                      <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                        <SearchIcon className="h-5 w-5" aria-hidden="true" />
+                      </div>
+                      <input
+                        id="desktop-search"
+                        className="block w-full bg-white bg-opacity-20 py-2 pl-10 pr-3 border rounded-md leading-5 text-gray-900 focus:outline-none focus:bg-opacity-100 focus:placeholder-gray-500 focus:ring-0 sm:text-sm"
+                        placeholder="Search"
+                        type="search"
+                        name="search"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
