a7362043632dbb92b0b828641a0e0d8176a7c396
Robert Guss <robertguss@pm.me>
2021-09-07 14:39:12 -0400

    made links to examples purple and removed get started buttons from rwe page

--- components/RealWorldExamples/Examples.tsx
+++ components/RealWorldExamples/Examples.tsx
@@ -35,14 +35,14 @@ export default function Examples({ sections, content, progressService }) {
                         content={content[section]}
                         progressService={progressService}
                       />
-                      <div className="mt-6">
+                      {/* <div className="mt-6">
                         <a
                           href={`/real-world-examples/${content[section].slug}`}
                           className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                         >
                           Get started
                         </a>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
@@ -80,14 +80,14 @@ export default function Examples({ sections, content, progressService }) {
                         content={content[section]}
                         progressService={progressService}
                       />
-                      <div className="mt-6">
+                      {/* <div className="mt-6">
                         <a
-                          href={content[section].slug}
+                          href={`/real-world-examples/${content[section].slug}`}
                           className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"
                         >
                           Get started
                         </a>
-                      </div>
+                      </div> */}
                     </div>
                   </div>
                 </div>
