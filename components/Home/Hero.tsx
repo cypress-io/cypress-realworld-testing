3ed8546cfbbda56cb3b87d9ab2d715f7b39e4ad9
Robert Guss <robertguss@pm.me>
2021-09-23 14:45:33 -0400

    updated hero features

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -6,11 +6,11 @@ const features = [
     description: "",
   },
   {
-    name: "40+ Lessons",
+    name: "25+ Lessons",
     description: "",
   },
   {
-    name: "30 Real World Examples",
+    name: "30+ Real World Examples",
     description: "",
   },
 ]
@@ -25,21 +25,6 @@ export default function Hero() {
               <div className="lg:grid lg:grid-cols-2 lg:gap-8">
                 <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
                   <div className="lg:py-24">
-                    {/* <a
-                      href="#"
-                      className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
-                    >
-                      <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">
-                        We're hiring
-                      </span>
-                      <span className="ml-4 text-sm">
-                        Visit our careers page
-                      </span>
-                      <ChevronRightIcon
-                        className="ml-2 w-5 h-5 text-gray-500"
-                        aria-hidden="true"
-                      />
-                    </a> */}
                     <h1
                       data-test="hero-heading"
                       className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl"
