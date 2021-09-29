6328de8f4d6eb1eddcc56f35abca1fb4515d1bd9
Robert Guss <robertguss@pm.me>
2021-09-29 11:26:08 -0400

    year in footer is dynamic

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -75,7 +75,7 @@ export default function Footer() {
           ))}
         </nav>
         <p className="mt-8 text-center text-base text-gray-400">
-          &copy; 2021 Cypress. All rights reserved.
+          &copy; {`${new Date().getFullYear()}`} Cypress. All rights reserved.
         </p>
       </div>
     </footer>
