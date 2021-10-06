9c3062fee6f7df57d65c87d5671b1fc52e8a9921
Robert Guss <robertguss@pm.me>
2021-10-06 11:47:21 -0400

    added screenshots to real world examples

--- components/RealWorldExamples/Examples.tsx
+++ components/RealWorldExamples/Examples.tsx
@@ -48,11 +48,10 @@ export default function Examples({ sections, content, progressService }) {
                 </div>
                 <div className="mt-12 sm:mt-16 lg:mt-0">
                   <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-                    {/* <img
+                    <img
                       className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                      src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
-                      alt="Inbox user interface"
-                    /> */}
+                      src={content[section].image}
+                    />
                   </div>
                 </div>
               </div>
@@ -93,11 +92,10 @@ export default function Examples({ sections, content, progressService }) {
                 </div>
                 <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
                   <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-                    {/* <img
+                    <img
                       className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                      src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
-                      alt="Customer profile user interface"
-                    /> */}
+                      src={content[section].image}
+                    />
                   </div>
                 </div>
               </div>
