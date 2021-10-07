1490ebc1d6e26d443036f3a270ffafc2b52bb6d2
Robert Guss <robertguss@pm.me>
2021-10-07 15:40:40 -0400

    removed commented code from RWE Hero comp

--- components/RealWorldExamples/RWEHero.tsx
+++ components/RealWorldExamples/RWEHero.tsx
@@ -1,15 +1,10 @@
-/* This example requires Tailwind CSS v2.0+ */
-
-export default function RealWorldExamplesHero() {
+export default function RWEHero() {
   return (
     <>
       <hr />
       <div className="relative pt-16 overflow-hidden sm:pt-24 lg:pt-20">
         <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
           <div>
-            {/* <h2 className="text-base font-semibold tracking-wider text-indigo-600 uppercase">
-              Serverless
-            </h2> */}
             <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
               Real World Examples
             </p>
