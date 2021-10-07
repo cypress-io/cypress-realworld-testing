752ce170f4ceabb12a98b24c33a0abb09beaad58
Robert Guss <robertguss@pm.me>
2021-10-07 13:44:11 -0400

    moved script to be inline

--- components/Lesson/LessonLayout.tsx
+++ components/Lesson/LessonLayout.tsx
@@ -70,7 +70,33 @@ export default function LessonLayout({
         </div>
       </div>
 
-      <Script src="/scripts/modal.js" />
+      <Script id="show-banner" strategy="afterInteractive">
+        {`
+          const images = document.querySelectorAll(".lesson-content img")
+          const modal = document.getElementById("modal")
+          const modalImg = document.querySelector("#modal .modal-content img")
+          const modalClose = document.querySelector("#modal .modal-content .close")
+
+          images.forEach((img) => {
+            img.addEventListener("click", (event) => {
+              let src = event.target.getAttribute("src")
+              modalImg.src = src
+              modal.style.display = "block"
+            })
+          })
+
+          modalClose.addEventListener("click", () => {
+            modal.style.display = "none"
+          })
+
+          // When the user clicks anywhere outside of the modal, close it
+          window.addEventListener("click", (event) => {
+            if (event.target == modal) {
+              modal.style.display = "none"
+            }
+          })
+        `}
+      </Script>
     </>
   )
 }
