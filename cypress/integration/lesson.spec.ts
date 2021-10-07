cc8e8e8a30a926c485b8689adc5fba15b4461077
Robert Guss <robertguss@pm.me>
2021-10-07 14:47:18 -0400

    removed test that checked TOC scrolling as it was not behaving consistently

--- cypress/integration/lesson.spec.ts
+++ cypress/integration/lesson.spec.ts
@@ -11,19 +11,6 @@ describe("Multiple Choice Challenge", () => {
     cy.getBySel("lesson-hero").should("not.exist")
   })
 
-  it.only("the TOC links to the correct content section when clicked", () => {
-    cy.getBySel("toc-sidebar").within(($toc) => {
-      cy.get("a").each(($link) => {
-        const href = $link.attr("href")
-        cy.wrap($link).click()
-
-        cy.window()
-          .its("scrollY")
-          .should("equal", Math.ceil(cy.$$(`${href}`).offset().top))
-      })
-    })
-  })
-
   it("shows the challenge", () => {
     cy.getBySel("multiple-choice-challenge").should("exist")
   })
