dad5fed739a97db17b26ccbc9dc7c16e2dde5ff0
Robert Guss <robertguss@pm.me>
2021-10-07 15:11:48 -0400

    fixed LessonBreadcrumbs filepath in Lesson Layout

--- components/Lesson/LessonLayout.tsx
+++ components/Lesson/LessonLayout.tsx
@@ -1,5 +1,5 @@
 import LessonToc from "../../components/Lesson/LessonToc"
-import LessonBreadcrumbs from "./LessonLessonBreadcrumbs"
+import LessonBreadcrumbs from "./LessonBreadcrumbs"
 import { MDXRemote } from "next-mdx-remote"
 import dynamic from "next/dynamic"
 const LessonProgress = dynamic(() => import("./LessonProgress"), {
