b7616f9731564ac733e7455c6ebf6b85aa3a7c5e
Robert Guss <robertguss@pm.me>
2021-10-07 15:40:05 -0400

    removed commented code from RWE Features comp

--- components/RealWorldExamples/RWEFeatures.tsx
+++ components/RealWorldExamples/RWEFeatures.tsx
@@ -1,4 +1,3 @@
-/* This example requires Tailwind CSS v2.0+ */
 import {
   DesktopComputerIcon,
   CogIcon,
@@ -42,7 +41,7 @@ const features = [
   },
 ]
 
-export default function Features() {
+export default function RWEFeatures() {
   return (
     <div className="features relative bg-white py-16 sm:py-24 lg:py-32">
       <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
@@ -52,11 +51,6 @@ export default function Features() {
         <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
           A quick preview of what you’ll learn from our examples
         </p>
-        {/* <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-          Phasellus lorem quam molestie id quisque diam aenean nulla in.
-          Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend
-          condimentum id viverra nulla.
-        </p> */}
         <div className="mt-12">
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {features.map((feature) => (
