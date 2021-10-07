6686d0b6401c0d45a6812177da4b28af18a27c01
Robert Guss <robertguss@pm.me>
2021-10-07 15:28:47 -0400

    removed commented code and unused props from HomeRealWorldExamples comp

--- components/Home/HomeRealWorldExamples.tsx
+++ components/Home/HomeRealWorldExamples.tsx
@@ -1,19 +1,12 @@
 import Link from "next/link"
 
-export default function HomeRealWorldExamples({
-  sections,
-  content,
-  progressService,
-}) {
+export default function HomeRealWorldExamples({}) {
   return (
     <>
       <hr />
       <div className="relative pt-16 overflow-hidden sm:pt-24 lg:pt-32">
         <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
           <div>
-            {/* <h2 className="text-base font-semibold tracking-wider text-indigo-600 uppercase">
-              Serverless
-            </h2> */}
             <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
               Over 30+ Real World Examples
             </p>
