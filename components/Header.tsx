c1ab3711cd7a926354294ae602dc3256c0d3f16a
Robert Guss <robertguss@pm.me>
2021-10-11 13:19:51 -0400

    updated <a> to use <Link> from next. Removed mobile nav code that was not in use.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
 import MobileNav from "./Mobile/MobileNav"
+import Link from "next/link"
 import {
   BookmarkAltIcon,
   BriefcaseIcon,
@@ -122,14 +123,16 @@ export default function Header({ content, sections, progressService }) {
         <div className="relative z-20">
           <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
             <div>
-              <a href="/" className="flex">
-                <span className="sr-only">Workflow</span>
-                <img
-                  className="h-8 w-auto sm:h-10"
-                  src="/images/logo/cypress-logo.webp"
-                  alt="Cypress Logo"
-                />
-              </a>
+              <Link href="/">
+                <a className="flex">
+                  <span className="sr-only">Workflow</span>
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                    src="/images/logo/cypress-logo.webp"
+                    alt="Cypress Logo"
+                  />
+                </a>
+              </Link>
             </div>
             <div className="md:hidden">
               <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
@@ -199,6 +202,7 @@ export default function Header({ content, sections, progressService }) {
                     </>
                   )}
                 </Popover>
+
                 <Popover>
                   {({ open }) => (
                     <>
@@ -314,6 +318,11 @@ export default function Header({ content, sections, progressService }) {
                     </>
                   )}
                 </Popover>
+                <Link href="/real-world-examples">
+                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Real World Examples
+                  </a>
+                </Link>
               </Popover.Group>
               <div className="flex items-center md:ml-12">
                 {/* Search */}
@@ -342,129 +351,6 @@ export default function Header({ content, sections, progressService }) {
             </div>
           </div>
         </div>
-
-        <Transition
-          as={Fragment}
-          enter="duration-200 ease-out"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="duration-100 ease-in"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          <Popover.Panel
-            focus
-            className="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-          >
-            <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-              <div className="pt-5 pb-6 px-5 sm:pb-8">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-mark-blue-600.svg"
-                      alt="Workflow"
-                    />
-                  </div>
-                  <div className="-mr-2">
-                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
-                      <span className="sr-only">Close menu</span>
-                      <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                <div className="mt-6 sm:mt-8">
-                  <nav>
-                    <div className="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                      {courses.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50"
-                        >
-                          <div className="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-blue-500 text-white sm:h-12 sm:w-12">
-                            <item.icon className="h-6 w-6" aria-hidden="true" />
-                          </div>
-                          <div className="ml-4 text-base font-medium text-gray-900">
-                            {item.name}
-                          </div>
-                        </a>
-                      ))}
-                    </div>
-                    <div className="mt-8 text-base">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 hover:text-blue-500"
-                      >
-                        {" "}
-                        View all products <span aria-hidden="true">&rarr;</span>
-                      </a>
-                    </div>
-                  </nav>
-                </div>
-              </div>
-              <div className="py-6 px-5">
-                <div className="grid grid-cols-2 gap-4">
-                  <a
-                    href="#"
-                    className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Pricing
-                  </a>
-
-                  <a
-                    href="#"
-                    className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Docs
-                  </a>
-
-                  <a
-                    href="#"
-                    className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Company
-                  </a>
-
-                  <a
-                    href="#"
-                    className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Resources
-                  </a>
-
-                  <a
-                    href="#"
-                    className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Blog
-                  </a>
-
-                  <a
-                    href="#"
-                    className="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    Contact Sales
-                  </a>
-                </div>
-                <div className="mt-6">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-blue-600 hover:bg-blue-700"
-                  >
-                    Sign up
-                  </a>
-                  <p className="mt-6 text-center text-base font-medium text-gray-500">
-                    Existing customer?{" "}
-                    <a href="#" className="text-blue-600 hover:text-blue-500">
-                      Sign in
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </Popover.Panel>
-        </Transition>
       </Popover>
       <MobileNav
         content={content}
