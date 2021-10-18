2f1c2f061e0792f7d065df33d718e91f35aa2692
Robert Guss <robertguss@pm.me>
2021-10-18 16:36:25 -0400

    added a spec file for complete lesson button. updated name of spec files to follow the same naming convention

--- components/Lesson/CompleteLessonBtn.tsx
+++ components/Lesson/CompleteLessonBtn.tsx
@@ -8,7 +8,7 @@ export default function NextLessonBtn({
   const [, progressSend] = useActor(progressService)
 
   return (
-    <div data-test="next-lesson-button" className="py-20">
+    <div data-test="complete-lesson-button" className="py-20">
       <a
         href={nextLessonPath ? nextLessonPath : "/"}
         className="mx-auto max-w-xl flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
