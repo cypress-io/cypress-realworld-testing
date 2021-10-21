8352b924071d3e8fa7e656c4c506884a2c12bbb6
Robert Guss <robertguss@pm.me>
2021-10-21 09:38:08 -0400

    added next example button to RWE lesson pages

--- components/RealWorldExamples/Lesson/RWENextLessonBtn.tsx
+++ components/RealWorldExamples/Lesson/RWENextLessonBtn.tsx
@@ -1,14 +1,11 @@
-export default function NextLessonBtn({ path, isCompleted }) {
+export default function NextLessonBtn({ path }) {
   return (
-    <div
-      data-test="next-lesson-button"
-      className={`${isCompleted ? "" : "hidden"} py-20`}
-    >
+    <div data-test="next-lesson-button" className="py-20">
       <a
-        href={path ? path : "/"}
+        href={path ? path : "/real-world-examples"}
         className="mx-auto max-w-xl flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
       >
-        {path ? "Next Lesson" : "Complete Section"}
+        {path ? "Next Example" : "Real World Examples"}
       </a>
     </div>
   )
