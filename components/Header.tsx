8bb55f26cf52463cbe84c4770d86c1da2bd85fa9
Robert Guss <robertguss@pm.me>
2021-10-27 15:32:44 -0400

    reverted Header comp to Header from main branch

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,9 +19,6 @@ import {
   TemplateIcon,
 } from "@heroicons/react/outline"
 import { ChevronDownIcon } from "@heroicons/react/solid"
-import { DocSearch } from "@docsearch/react"
-
-import "@docsearch/css"
 
 const company = [
   {
@@ -303,12 +300,27 @@ export default function Header({ content, sections, progressService }) {
               </Popover.Group>
               <div className="flex items-center md:ml-12">
                 {/* Search */}
-                <DocSearch
-                  appId="8V1MWQUQBN"
-                  apiKey="9ed61950eb9df441b30a47fcdd423a48"
-                  indexName="real_world_testing"
-                  disableUserPersonalization={true}
-                />
+                <div className="md:flex items-center justify-end w-full max-w-sm">
+                  <div className="flex-1 min-w-0">
+                    <div className="w-full">
+                      <label htmlFor="desktop-search" className="sr-only">
+                        Search
+                      </label>
+                      <div className="relative focus-within:text-gray-600">
+                        <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                          <SearchIcon className="h-5 w-5" aria-hidden="true" />
+                        </div>
+                        <input
+                          id="desktop-search"
+                          className="block w-full bg-white bg-opacity-20 py-2 pl-10 pr-3 border rounded-md leading-5 text-gray-900 focus:outline-none focus:bg-opacity-100 focus:placeholder-gray-500 focus:ring-0 sm:text-sm"
+                          placeholder="Search"
+                          type="search"
+                          name="search"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
