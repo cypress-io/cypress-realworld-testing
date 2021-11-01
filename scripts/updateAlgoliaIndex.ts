7c86da1c8c5a41760bbaf86c2047930e925c879e
Robert Guss <robertguss@pm.me>
2021-11-01 13:46:59 -0400

    updated updateAlgolia script to use the app id from as an env var

--- scripts/updateAlgoliaIndex.ts
+++ scripts/updateAlgoliaIndex.ts
@@ -2,7 +2,10 @@ import algoliasearch from "algoliasearch"
 import searchIndex from "../public/search-index.json"
 require("dotenv").config()
 
-const client = algoliasearch("8V1MWQUQBN", process.env.ALGOLIA_ADMIN_KEY)
+const client = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+  process.env.ALGOLIA_ADMIN_KEY
+)
 
 const index = client.initIndex("real_world_testing")
 
