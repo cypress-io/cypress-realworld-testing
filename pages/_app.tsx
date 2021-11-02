97cd7d7c78d8c037aecc750bc77e9125031c974c
Robert Guss <robertguss@pm.me>
2021-11-02 16:06:20 -0400

    removed full story import form _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "tailwindcss/tailwind.css"
 import "../styles/global.css"
 import Head from "next/head"
 import Script from "next/script"
-import * as FullStory from "@fullstory/browser"
 
 function MyApp({ Component, pageProps }) {
   return (
