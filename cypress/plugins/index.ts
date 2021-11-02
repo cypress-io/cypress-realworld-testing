982a8ede813e4b27af05acc2600da79c78430cfb
Robert Guss <robertguss@pm.me>
2021-11-02 13:29:57 -0400

    regst

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,13 +1,10 @@
 /// <reference types="cypress" />
-// @ts-ignore
-require("dotenv").config()
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 export default (on, config) => {
-  config.env.siteURL = process.env.SITE_URL
-
-  return config
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
 }
