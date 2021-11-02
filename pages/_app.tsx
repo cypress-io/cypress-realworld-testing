a003116c8cd23bb47a9b8634d85699549c0d2b56
Robert Guss <robertguss@pm.me>
2021-11-02 14:40:06 -0400

    added fullstory via react useEffect hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import "tailwindcss/tailwind.css"
 import "../styles/global.css"
 import Head from "next/head"
+import { useEffect } from "react"
 import * as FullStory from "@fullstory/browser"
 
-if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
-  FullStory.init({ orgId: process.env.NEXT_PUBLIC_FULL_STORY_ORG_ID })
-}
-
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    FullStory.init({ orgId: process.env.NEXT_PUBLIC_FULL_STORY_ORG_ID })
+  }, [])
   return (
     <>
       <Head>
