d6a1b9fe04ef25102a7075d21949597bf2148e33
Robert Guss <robertguss@pm.me>
2021-11-12 11:53:17 -0500

    added more styles to sidebar

--- components/Lesson/LessonLayout.tsx
+++ components/Lesson/LessonLayout.tsx
@@ -32,7 +32,7 @@ export default function LessonLayout({
         <div className="py-6">
           <div className="max-w-3xl mx-auto sm:px-6 lg:max-w-full lg:px-8 lg:grid lg:grid-cols-12 lg:gap-8">
             {/* Table of Content */}
-            <div className="hidden lg:block lg:col-span-3 xl:col-span-3">
+            <div className="hidden lg:block lg:col-span-3 xl:col-span-2">
               <div className="sticky top-6">
                 <p className="font-semibold mb-4">ON THIS PAGE</p>
                 <LessonSidebar
@@ -45,7 +45,7 @@ export default function LessonLayout({
             </div>
 
             {/* Content */}
-            <main className="lesson-content lg:col-span-9 xl:col-span-6">
+            <main className="lesson-content lg:col-span-9 xl:col-span-7">
               <div className="relative bg-white overflow-hidden">
                 <div className="relative px-4 sm:px-6 lg:px-8">
                   <div className="prose prose-indigo prose-lg text-gray-500 mx-auto">
