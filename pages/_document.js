2ad45fd726c316aa16ae03563bfe54cdc7fd6bb5
Robert Guss <robertguss@pm.me>
2021-11-17 15:41:15 -0500

    updated header logo and added home page hero features and screenshot

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin={true}
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
