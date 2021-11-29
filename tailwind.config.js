d041fbb6aa27902012a421ae9f35eda22fabd7ec
Robert Guss <robertguss@pm.me>
2021-11-29 10:54:46 -0500

    turned on tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  // mode: "jit",
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
