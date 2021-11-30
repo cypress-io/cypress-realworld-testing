f0f98edaccf54dafb438ecb3e2be150d81f507c1
Robert Guss <robertguss@pm.me>
2021-11-30 15:44:56 -0500

    changed color of progress line

--- components/Progress/ProgressLine.tsx
+++ components/Progress/ProgressLine.tsx
@@ -10,7 +10,7 @@ export default function ProgressLine({ index, isCompleted, lessons }: Props) {
       {index !== lessons.length - 1 ? (
         <div
           className={`-ml-px absolute mt-0.5 top-4 left-4 w-0.5 h-full ${
-            isCompleted ? "bg-indigo-600" : "bg-gray-300"
+            isCompleted ? "bg-jade-300" : "bg-gray-300"
           }`}
           aria-hidden="true"
         />
