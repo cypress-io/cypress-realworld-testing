a5073e94b2a60312a476a7dad1acbf7537417d04
Robert Guss <robertguss@pm.me>
2021-11-30 09:29:41 -0500

    added back the Real World Examples component and used dynamic loading to fix missing tailwind classes bug

--- cypress/integration/real-world-examples.spec.ts
+++ cypress/integration/real-world-examples.spec.ts
@@ -9,20 +9,19 @@ describe("RWE Landing Page", function () {
   it("renders all of the Real World Examples titles & lessons", function () {
     const rwaExamples = Object.keys(realWorldExamples)
 
-    _.each(rwaExamples, (example) => {
-      console.log(example)
-      const title = realWorldExamples[example].title
-      const lessons = realWorldExamples[example].lessons
+    _.each(rwaExamples, (lesson, index) => {
+      const title = realWorldExamples[lesson].title
+      const lessons = realWorldExamples[lesson].lessons
 
-      cy.getBySel(`real-world-example-${example}`).within(() => {
-        cy.getBySel(`${example}-title`).contains(title)
+      cy.getBySel(`real-world-example-${index}`).within(() => {
+        cy.getBySel(`category-${index}-title`).contains(title)
 
         _.each(lessons, (lesson, index) => {
           const lessonTitle = lessons[index].title
           const lessonDescription = lessons[index].description
 
-          cy.getBySel(`${example}-${index}-title`).contains(lessonTitle)
-          cy.getBySel(`${example}-${index}-description`).contains(
+          cy.getBySel(`real-world-example-${index}-title`).contains(lessonTitle)
+          cy.getBySel(`real-world-example-${index}-description`).contains(
             lessonDescription
           )
         })
