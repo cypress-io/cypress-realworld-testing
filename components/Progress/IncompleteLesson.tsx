fd344d551d9592a00cd6258378777f6ca0ca7eaf
Robert Guss <robertguss@pm.me>
2022-02-25 13:57:40 -0500

    removed progress line component. reduced size of progress circles to h-6 w-6

--- components/Progress/IncompleteLesson.tsx
+++ components/Progress/IncompleteLesson.tsx
@@ -8,7 +8,7 @@ export default function IncompleteLesson({ index }: Props) {
       <span className="flex h-9 items-center" aria-hidden="true">
         <span
           data-test={`lesson-upcoming-${index}`}
-          className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400"
+          className="relative z-10 flex h-6 w-6 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400"
         >
           <span className="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-gray-300" />
         </span>
