d868578c52b90627ba4dce3ab1b4316af27b46d2
Robert Guss <robertguss@pm.me>
2022-02-25 10:59:46 -0500

    made all examples a clickable link

--- components/RealWorldExamples/RWEProgress.tsx
+++ components/RealWorldExamples/RWEProgress.tsx
@@ -7,32 +7,32 @@ export default function RWEProgress({ content }) {
 
       <ol className="overflow-hidden">
         {content?.lessons.map((lesson, index) => (
-          <li
-            data-test={`lesson-${index}`}
-            key={lesson.title}
-            className="pb-10"
+          <a
+            href={`/real-world-examples/${lesson.slug}`}
+            data-test={`real-world-lesson-${index}`}
+            key={index}
           >
-            <div className="group relative flex items-start">
-              <span className="flex min-w-0 flex-col">
-                <a
-                  href={`/real-world-examples/${lesson.slug}`}
-                  data-test={`real-world-lesson-${index}`}
-                  className="mb-2 text-lg font-semibold uppercase tracking-wide text-gray-700"
-                >
-                  {lesson.title}
-                </a>
+            <li data-test={`lesson-${index}`} className="pb-10">
+              <div className="group relative flex items-start">
+                <span className="flex min-w-0 flex-col">
+                  <p className="mb-2 text-lg font-semibold uppercase tracking-wide text-gray-700">
+                    {lesson.title}
+                  </p>
 
-                <span className="text text-gray-500">{lesson.description}</span>
-              </span>
+                  <span className="text text-gray-500">
+                    {lesson.description}
+                  </span>
+                </span>
 
-              <img
-                src="/images/real-world-examples/icons/right-arrow.svg"
-                alt="right arrow icon"
-                className="absolute right-2 top-4"
-              />
-            </div>
-            <div className="mt-4 border" />
-          </li>
+                <img
+                  src="/images/real-world-examples/icons/right-arrow.svg"
+                  alt="right arrow icon"
+                  className="absolute right-2 top-4"
+                />
+              </div>
+              <div className="mt-4 border" />
+            </li>
+          </a>
         ))}
       </ol>
     </nav>
