ebf0fa978a9ac113e8d5e0e7d166d74282237a83
Robert Guss <robertguss@pm.me>
2022-03-07 16:33:43 -0500

    updated tutorials hero

--- components/Tutorials/TutorialsHero.tsx
+++ components/Tutorials/TutorialsHero.tsx
@@ -1,16 +1,45 @@
+import Image from "next/image"
+
 export default function TutorialsHero() {
   return (
     <>
-      <div className="relative h-full overflow-hidden">
-        <div className="h-[36rem] bg-rwe-hero bg-cover bg-center bg-no-repeat">
-          <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-            <div>
-              <p className="pt-24 text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl lg:text-[40px]">
-                Tutorials
-              </p>
+      <div className="relative overflow-hidden">
+        <main>
+          <div className="bg-jade-300 bg-course-hero bg-cover bg-center bg-no-repeat py-28">
+            <div className="mx-auto w-full lg:pl-8">
+              <div className="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left">
+                  <div className="lg:py-24">
+                    <h1
+                      data-test="hero-heading"
+                      className="mt-4 text-5xl font-bold leading-tight text-gray-1000"
+                    >
+                      Tutorials
+                    </h1>
+                    {/* <p className="mt-3 text-base text-jade-700 sm:mt-5 sm:text-xl lg:text-lg xl:text-2xl">
+                      Learn cool stuff.
+                    </p> */}
+                  </div>
+                </div>
+
+                <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+                  <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+                    <div className="hidden sm:block"></div>
+                    <div className="relative -mr-40 pl-4 sm:mx-auto sm:max-w-3xl sm:px-0 lg:h-full lg:max-w-none lg:pl-12">
+                      <Image
+                        className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                        src="/images/tutorials/next-js-and-shopify-store/creating-a-shopify-partners-store/Screen_Shot_2021-12-16_at_9.25.15_AM.png"
+                        alt="Course Page Hero Image"
+                        width={917}
+                        height={518}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </main>
       </div>
     </>
   )
