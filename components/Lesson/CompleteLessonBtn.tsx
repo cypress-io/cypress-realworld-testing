de9cdca4c796768942fa671e6907201aa6f1fc49
Robert Guss <robertguss@pm.me>
2022-03-07 16:22:22 -0500

    changed styles and colors of lesson quizzes and buttons

--- components/Lesson/CompleteLessonBtn.tsx
+++ components/Lesson/CompleteLessonBtn.tsx
@@ -17,7 +17,7 @@ export default function NextLessonBtn({
     <div data-test="complete-lesson-button" className="py-20">
       <a
         href={nextLessonPath ? nextLessonPath : "/"}
-        className="mx-auto flex max-w-xl items-center justify-center rounded-md bg-teal-500 px-8 py-3 text-base font-medium text-white  md:py-4 md:px-10 md:text-lg"
+        className="mx-auto flex max-w-xl items-center justify-center rounded-md bg-indigo-500 px-8 py-3 text-base font-medium text-white  md:py-4 md:px-10 md:text-lg"
         onClick={() => {
           progressSend({
             type: "COMPLETE_LESSON",
