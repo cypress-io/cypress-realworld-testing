8c16a612b03e9e2594b3ccf292a20cfa86d1f4a3
Robert Guss <robertguss@pm.me>
2022-06-02 10:29:26 -0400

    ran v10 migration

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  viewportHeight: 1000,
+  viewportWidth: 1280,
+  env: {
+    mobileViewportWidthBreakpoint: 414,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.ts').default(on, config)
+    },
+    baseUrl: 'http://localhost:3000',
+  },
+})
