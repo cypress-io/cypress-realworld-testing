1b28b4f2e6c205eda6235a37dc0bae4fd37a4aa9
Robert Guss <robertguss@pm.me>
2022-07-12 14:16:08 -0400

    fixed typos in how to test forms lesson

--- components/Disclosure.tsx
+++ components/Disclosure.tsx
@@ -8,7 +8,7 @@ export default function Example({ children }) {
         {({ open }) => (
           <>
             <Disclosure.Button className="flex w-full justify-between rounded-lg bg-gray-100 px-4 py-2 text-left text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500 focus-visible:ring-opacity-75">
-              <span>Practice Answers</span>
+              <span>Practice Answer</span>
               <ChevronUpIcon
                 className={`${
                   open ? "rotate-180 transform" : ""
